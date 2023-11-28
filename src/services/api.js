d199d2afe68709cc84debc6e3325848ea25be867
ThuongDoo <domanhthuong20122002@egmail.com>
2023-11-28 07:54:37 +0700

    change api link

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "http://192.168.0.118:4000/api/v1",
+  baseURL: "http://localhost:4000/api/v1",
 });
 
 export default instance;
